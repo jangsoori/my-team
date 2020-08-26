c438f1b562f5a0cccb404396ade301eed4fb37df
Gracjan Kołodziej <gracjan@MBP-Gracjan.lan>
2020-08-26 12:20:12 +0100

    added Content component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./header/Header";
 import "./App.scss";
+import Content from "./content_routes/Content";
 
 export default function App() {
   return (
     <div className="app bg-main">
       <Header />
+      <Content />
     </div>
   );
 }
