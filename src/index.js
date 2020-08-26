e3a9a67eef679c8a8e9d811bd98510808741ee76
Gracjan Kołodziej <gracjan@MBP-Gracjan.lan>
2020-08-26 10:54:24 +0100

    Added router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+//Router
+import { BrowserRouter as Router } from "react-router-dom";
+
 //Components
 import App from "./components/App";
 
@@ -9,4 +12,9 @@ import "normalize.css";
 import "./reset.css";
 import "./index.scss";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
