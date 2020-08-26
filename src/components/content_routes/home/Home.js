aa5560d69224081867af89c8ad1ec9649093ade8
Gracjan Kołodziej <gracjan@MBP-Gracjan.lan>
2020-08-26 12:19:29 +0100

    Started working on home page

---
+++ src/components/content_routes/home/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Home.scss";
+import "../../../utils/typography.scss";
+export default function Home() {
+  return (
+    <section className="content home">
+      <h2 className="content-heading heading-medium">
+        Find the best <span className="text-empaphise">talent</span>
+      </h2>
+      <p className="content-text body-text-secondary">
+        Finding the right people and building high performing teams can be hard.
+        Most companies aren’t tapping into the abundance of global talent. We’re
+        about to change that.
+      </p>
+    </section>
+  );
+}
