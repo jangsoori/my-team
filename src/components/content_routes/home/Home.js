16893b4660a8958003b2d5d22c10cff4b603376a
Gracjan Kołodziej <gracjan@MBP-Gracjan.lan>
2020-08-26 21:58:21 +0100

    Added SVG and red line according to design

--- src/components/content_routes/home/Home.js
+++ src/components/content_routes/home/Home.js
@@ -51,7 +51,44 @@ export default function Home() {
         </svg>
       </section>
       <section className="home-section-2">
+        <svg
+          className="home-svg-2"
+          width="100"
+          height="244"
+          viewBox="0 0 100 244"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect
+            x="100"
+            y="144"
+            width="100"
+            height="100"
+            transform="rotate(-180 100 144)"
+            fill="#2C6269"
+          />
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M100 244C155.228 244 200 199.228 200 144C200 88.7715 155.228 44 100 44C44.7715 44 0 88.7715 0 144C0 199.228 44.7715 244 100 244ZM100 177C118.225 177 133 162.225 133 144C133 125.775 118.225 111 100 111C81.7746 111 67 125.775 67 144C67 162.225 81.7746 177 100 177Z"
+            fill="#014E56"
+          />
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M100 144H0V148H100V144ZM100 152H0V156H100V152ZM0 160H100V164H0V160ZM100 168H0V172H100V168ZM0 176H100V180H0V176ZM100 184H0V188H100V184Z"
+            fill="#79C8C7"
+          />
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M0 0H4V4H0V0ZM11 0H7V4H11V0ZM11 8H7V12H11V8ZM7 16H11V20H7V16ZM11 24H7V28H11V24ZM7 32H11V36H7V32ZM11 40H7V44H11V40ZM4 8H0V12H4V8ZM18 0H14V4H18V0ZM18 8H14V12H18V8ZM14 16H18V20H14V16ZM18 24H14V28H18V24ZM14 32H18V36H14V32ZM18 40H14V44H18V40ZM4 16H0V20H4V16ZM25 0H21V4H25V0ZM25 8H21V12H25V8ZM21 16H25V20H21V16ZM25 24H21V28H25V24ZM21 32H25V36H21V32ZM25 40H21V44H25V40ZM4 24H0V28H4V24ZM32 0H28V4H32V0ZM32 8H28V12H32V8ZM28 16H32V20H28V16ZM32 24H28V28H32V24ZM28 32H32V36H28V32ZM32 40H28V44H32V40ZM4 32H0V36H4V32ZM39 0H35V4H39V0ZM39 8H35V12H39V8ZM35 16H39V20H35V16ZM39 24H35V28H39V24ZM35 32H39V36H35V32ZM39 40H35V44H39V40ZM4 40H0V44H4V40ZM46 0H42V4H46V0ZM46 8H42V12H46V8ZM42 16H46V20H42V16ZM46 24H42V28H46V24ZM42 32H46V36H42V32ZM46 40H42V44H46V40ZM49 0H53V4H49V0ZM49 8H53V12H49V8ZM53 16H49V20H53V16ZM49 24H53V28H49V24ZM53 32H49V36H53V32ZM49 40H53V44H49V40Z"
+            fill="#79C8C7"
+          />
+        </svg>
+
         <h2 className="content-heading heading-mobile-secondary">
+          <span className="red-line"></span>
           Build & manage distributed teams like no one else.
         </h2>
       </section>
