3f76011891d7399f9b3f26b2e90d886112a00b13
Gracjan Kołodziej <gracjan@MBP-Gracjan.lan>
2020-08-27 21:41:06 +0100

    Started working on GetStarted section

---
+++ src/components/GetStarted.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./GetStarted.scss";
+export default function GetStarted() {
+  return (
+    <section className="get-started grid">
+      <h2 className="heading-mobile-secondary get-started-title">
+        Ready to get started?
+      </h2>
+      <button className="btn btn-primary-dark">contact us</button>
+    </section>
+  );
+}
