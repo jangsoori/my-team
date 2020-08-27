c8a98aa017f52aa7a9af3005e8334f8d5b2a3fdf
Gracjan Kołodziej <gracjan@MBP-Gracjan.lan>
2020-08-27 21:39:38 +0100

    added routing

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,19 @@ import React from "react";
 import Header from "./header/Header";
 import "./App.scss";
 import Content from "./content_routes/Content";
+import { Route } from "react-router-dom";
+import GetStarted from "./GetStarted";
+import Footer from "./footer/Footer";
+import "../utils/buttons.scss";
+import "../utils/typography.scss";
 
 export default function App() {
   return (
     <div className="app">
       <Header />
       <Content />
+      <Route exact path={["/", "/about"]} component={GetStarted} />
+      <Footer />
     </div>
   );
 }
