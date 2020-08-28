2ab16c7d4498a451270a956ec9ef929451df2742
Gracjan Kołodziej <gracjan@MBP-Gracjan.lan>
2020-08-28 21:24:59 +0100

    start working on team section

--- src/components/content_routes/about/Team.js
+++ src/components/content_routes/about/Team.js
@@ -1,11 +1,30 @@
 import React from "react";
 import "./Team.scss";
+import TeamItem from "./TeamItem";
+
+const team = [
+  {
+    name: "Nikita Marks",
+    occupation: "Founder & CEO",
+    picture: "",
+    quote:
+      "“Empowered teams create truly amazing products. Set the north star and let them follow it.”",
+    socials: { twitter: "", facebook: "" },
+  },
+];
+
+const renderTeam = (team) => {
+  return team.map((item) => {
+    return <TeamItem item={item} />;
+  });
+};
 export default function Team() {
   return (
     <React.Fragment>
       <h2 className="content-heading heading-mobile-secondary about-team-title">
         Meet the directors
       </h2>
+      <ul className="team-list">{renderTeam(team)}</ul>
     </React.Fragment>
   );
 }
---
+++ src/components/content_routes/about/TeamItem.js
@@ -0,0 +1,5 @@
+import React from "react";
+import "./TeamItem.scss";
+export default function TeamItem() {
+  return <figure className="team-item"></figure>;
+}
