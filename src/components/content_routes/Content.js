056da1e999d04269c35493c42c44d2636a2a9be2
Gracjan Kołodziej <gracjan@MBP-Gracjan.lan>
2020-08-28 13:25:25 +0100

    added route for about page

--- src/components/content_routes/Content.js
+++ src/components/content_routes/Content.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import "./Content.scss";
 import Home from "./home/Home";
+import About from "./about/About";
 export default function Content() {
   return (
     <section className="content-wrapper bg-main">
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
       </Switch>
     </section>
   );
