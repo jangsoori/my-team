64625a92312a0f5c0b47e8115d74d43cbb7388f1
Gracjan Kołodziej <swider96@gmail.com>
2020-08-30 13:00:52 +0100

    added route for Contact page

--- src/components/content_routes/Content.js
+++ src/components/content_routes/Content.js
@@ -3,12 +3,14 @@ import { Route, Switch } from "react-router-dom";
 import "./Content.scss";
 import Home from "./home/Home";
 import About from "./about/About";
+import Contact from "./contact/Contact";
 export default function Content() {
   return (
     <section className="content-wrapper bg-main">
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/about" component={About} />
+        <Route exact path="/contact" component={Contact} />
       </Switch>
     </section>
   );
