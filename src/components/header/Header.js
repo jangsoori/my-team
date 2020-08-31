932f4420d498e3f138ef38d7371c60f0202bc4fd
Gracjan Kołodziej <swider96@gmail.com>
2020-08-31 14:01:58 +0100

    Added declarative rendering depening on size of the viewport

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,12 +3,20 @@ import "./Header.scss";
 import Logo from "./Logo";
 import "../../utils/utilityClasses.scss";
 import Navbar from "./Navbar";
-
+import useWindowSize from "../../hooks/useWindowSize";
+import NavDesktop from "./NavDesktop";
 export default function Header() {
+  const size = useWindowSize();
+  console.log(size.width);
   return (
-    <header className="header grid bg-main">
+    <header
+      className={`header grid bg-main ${
+        size.width > 600 ? "header-desktop" : ""
+      }`}
+    >
       <Logo />
-      <Navbar />
+      {/* Detect screen size, if wider than 600px, render desktop navbar  */}
+      {size.width > 600 ? <NavDesktop /> : <Navbar />}
     </header>
   );
 }
