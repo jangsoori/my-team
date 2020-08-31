fa34f0e425448662e48bc7367cbd1913dae7d49c
Gracjan Kołodziej <swider96@gmail.com>
2020-08-31 15:30:16 +0100

    added declarative rendering for logo if screen wider than 1281px

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,6 +5,7 @@ import "../../utils/utilityClasses.scss";
 import Navbar from "./Navbar";
 import useWindowSize from "../../hooks/useWindowSize";
 import NavDesktop from "./NavDesktop";
+import Logo_big from "./Logo_big";
 export default function Header() {
   const size = useWindowSize();
   console.log(size.width);
@@ -14,7 +15,8 @@ export default function Header() {
         size.width > 600 ? "header-desktop" : ""
       }`}
     >
-      <Logo />
+      {size.width > 1281 ? <Logo_big /> : <Logo />}
+
       {/* Detect screen size, if wider than 600px, render desktop navbar  */}
       {size.width > 600 ? <NavDesktop /> : <Navbar />}
     </header>
