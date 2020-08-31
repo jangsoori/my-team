306210afa9581e21d1854be6d0cfe7bde0ba2064
Gracjan Kołodziej <swider96@gmail.com>
2020-08-31 15:29:43 +0100

    added new svg and blue line for desktop size

--- src/components/content_routes/home/Home.js
+++ src/components/content_routes/home/Home.js
@@ -6,14 +6,44 @@ export default function Home() {
   return (
     <React.Fragment>
       <section className="content home-main bg-main">
-        <h2 className="content-heading heading-main">
+        <span className="blue-line"></span>
+
+        <h2 className="content-heading heading-main heading-home">
           Find the best <span className="text-emphasize">talent</span>
         </h2>
-        <p className="content-text body-text-secondary">
+        <p className="content-text body-text-secondary body-text-home">
           Finding the right people and building high performing teams can be
           hard. Most companies aren’t tapping into the abundance of global
           talent. We’re about to change that.
         </p>
+        <svg
+          className="home-svg-5"
+          width="100"
+          height="200"
+          viewBox="0 0 100 200"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect
+            width="100"
+            height="100"
+            transform="matrix(1 0 0 -1 0 200)"
+            fill="#79C8C7"
+          />
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M0 200C-55.2285 200 -100 155.228 -100 100C-100 44.7715 -55.2285 0 0 0C55.2285 0 100 44.7715 100 100C100 155.228 55.2285 200 0 200ZM0 133C-18.2254 133 -33 118.225 -33 100C-33 81.7746 -18.2254 67 0 67C18.2254 67 33 81.7746 33 100C33 118.225 18.2254 133 0 133Z"
+            fill="#012F34"
+          />
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M100 156H96V160H100V156ZM89 156H93V160H89V156ZM89 164H93V168H89V164ZM93 172H89V176H93V172ZM89 180H93V184H89V180ZM93 188H89V192H93V188ZM89 196H93V200H89V196ZM96 164H100V168H96V164ZM82 156H86V160H82V156ZM82 164H86V168H82V164ZM86 172H82V176H86V172ZM82 180H86V184H82V180ZM86 188H82V192H86V188ZM82 196H86V200H82V196ZM96 172H100V176H96V172ZM75 156H79V160H75V156ZM75 164H79V168H75V164ZM79 172H75V176H79V172ZM75 180H79V184H75V180ZM79 188H75V192H79V188ZM75 196H79V200H75V196ZM96 180H100V184H96V180ZM68 156H72V160H68V156ZM68 164H72V168H68V164ZM72 172H68V176H72V172ZM68 180H72V184H68V180ZM72 188H68V192H72V188ZM68 196H72V200H68V196ZM96 188H100V192H96V188ZM61 156H65V160H61V156ZM61 164H65V168H61V164ZM65 172H61V176H65V172ZM61 180H65V184H61V180ZM65 188H61V192H65V188ZM61 196H65V200H61V196ZM96 196H100V200H96V196ZM54 156H58V160H54V156ZM54 164H58V168H54V164ZM58 172H54V176H58V172ZM54 180H58V184H54V180ZM58 188H54V192H58V188ZM54 196H58V200H54V196ZM51 156H47V160H51V156ZM51 164H47V168H51V164ZM47 172H51V176H47V172ZM51 180H47V184H51V180ZM47 188H51V192H47V188ZM51 196H47V200H51V196Z"
+            fill="#F67E7E"
+          />
+        </svg>
+
         <svg
           className="home-svg-1"
           width="358"
